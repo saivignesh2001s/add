6085513b4f37672740c6c8f3279b08bed5c2f2bc
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-20 13:16:40 +0530

    Add project files.

---
+++ add/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace add
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a, b;
+            a = 10;
+            b = 20;
+            int c;
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+        }
+    }
+}
